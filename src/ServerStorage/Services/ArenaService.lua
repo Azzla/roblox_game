6886e42cc81e4df9b6cce7b190d5e4ad160ac738
Carson <azzlamusic@hotmail.com>
2021-12-26 23:53:51 -0700

    added ArenaService

---
+++ src/ServerStorage/Services/ArenaService.lua
@@ -0,0 +1,21 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Knit = require(ReplicatedStorage.Packages.Knit)
+
+local ArenaService = Knit.CreateService {
+    Name = "ArenaService",
+    Client = {}
+}
+
+function ArenaService:StartGame()
+    
+end
+
+function ArenaService:KnitStart()
+    
+end
+
+function ArenaService:KnitInit()
+
+end
+
+return ArenaService
